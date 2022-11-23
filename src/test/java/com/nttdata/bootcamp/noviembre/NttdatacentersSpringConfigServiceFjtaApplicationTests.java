ad03b210dc77e7ccc238f71f124b2fa70f5e4cb5
javitar02 <fjavitorrejon25@gmail.com>
2022-11-23 12:40:00 +0100

    Subido

---
+++ src/test/java/com/nttdata/bootcamp/noviembre/NttdatacentersSpringConfigServiceFjtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.noviembre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdatacentersSpringConfigServiceFjtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
